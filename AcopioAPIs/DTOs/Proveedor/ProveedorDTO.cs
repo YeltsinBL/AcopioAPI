957a13e09f866a12bfabcc0b37d6168cb8edaaf6
Guillermo Yeltsin Baltodano León <gbl_leo_30@hotmail.com>
2024-11-27 03:29:46 -0500

    Creación de ProveedorDTO

---
+++ AcopioAPIs/DTOs/Proveedor/ProveedorDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace AcopioAPIs.DTOs.Proveedor
+{
+    public class ProveedorDTO
+    {
+        public int ProveedorId { get; set; }
+        public string ProveedorUT { get; set; }
+        public string PersonDNI { get; set; }
+        public string PersonName { get; set; }
+        public string PersonPaternalSurname { get; set; }
+        public string PersonMaternalSurname { get; set; }
+        public bool ProveedorStatus { get; set; }
+    }
+}
