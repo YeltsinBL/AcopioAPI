bf36ea7fd06cda5b1761416d4006c6cc8504605f
Guillermo Yeltsin Baltodano León <gbl_leo_30@hotmail.com>
2024-11-27 03:29:17 -0500

    Modificar ProveedorResultDto

--- AcopioAPIs/DTOs/Proveedor/ProveedorResultDto.cs
+++ AcopioAPIs/DTOs/Proveedor/ProveedorResultDto.cs
@@ -2,15 +2,11 @@
 {
     public class ProveedorResultDto
     {
-        public int PersonId { get; set; }
-        public string PersonDNI { get; set; }
-        public string PersonName { get; set; }
-        public string PersonPaternalSurname { get; set; }
-        public string PersonMaternalSurname { get; set; }
         public int ProveedorId { get; set; }
         public string ProveedorUT { get; set; }
-        public int ProveedorStatus { get; set; }
-        public DateTime UserCreatedAt { get; set; }
+        public string PersonDNI { get; set; }
+        public string ProveedorNombre { get; set; }
+        public bool ProveedorStatus { get; set; }
     }
 
 }
